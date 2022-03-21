1b97b4f3ca4aeb3dda87e720726530f5d8cc221b
Hassan Andrabi <handrabi53@gmail.com>
2022-03-21 11:12:42 +1100

    Added JS to staff list. Added CoVStrain as external link. Removed 2022 will be better frl be better report from visualisations section.

--- src/_data/external_links.js
+++ src/_data/external_links.js
@@ -1,19 +1,30 @@
-
 exports.content = [
-	{
-		tags: ["post"],
+    {
+        tags: ["post"],
 
-		section: "Public Health Interventions",
-		type: "Interactive",
-	
-		title: "Australia New Zealand Health Intervention League Table (ANZ-HILT)",
-		date: "2019-08-05",
-		date_updated: "2019-08-05",
+        section: "Public Health Interventions",
+        type: "Interactive",
 
-		url: "https://league-table.shinyapps.io/bode3/",
-		
-		hidden: false
-	}
+        title: "Australia New Zealand Health Intervention League Table (ANZ-HILT)",
+        date: "2019-08-05",
+        date_updated: "2019-08-05",
+
+        url: "https://league-table.shinyapps.io/bode3/",
+
+        hidden: false,
+    },
+    {
+        tags: ["post"],
+
+        section: "COVID-19 Pandemic",
+        type: "Interactive",
+
+        title: "CoVStrain: COVID-19 sequencing prevalences around the world",
+        date: "2022-03-21",
+        date_updated: "2022-03-21",
+
+        url: "https://covstrain.com",
+
+        hidden: false,
+    },
 ];
-
-
